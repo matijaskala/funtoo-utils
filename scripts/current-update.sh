e40475862129d2969480476a792912fa24241c39
Matija Skala <mskala@gmx.com>
2017-02-05 18:20:20 +0100

    remove some unneccessary files

--- scripts/current-update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-eval `keychain --noask --eval id_dsa`  || exit 1
-# get latest merge.py and friends
-git pull || exit 1
-./merge.py /var/git/ports-2013 || exit 1
--- scripts/experimental-update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-eval `keychain --noask --eval id_dsa`  || exit 1
-# get latest merge.py and friends
-git pull || exit 1
-./merge.py --branch experimental /var/git/experimental-mini-2011 || exit 1
